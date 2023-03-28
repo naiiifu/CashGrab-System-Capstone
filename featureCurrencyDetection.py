8dead6ec41aea01d97d3fadd7f3525220722cc02
Dakota Crozier <45215796+2018dakotac@users.noreply.github.com>
2023-03-28 14:04:38 -0700

    adding stuff

--- featureCurrencyDetection.py
+++ featureCurrencyDetection.py
@@ -18,41 +18,25 @@ def validateSift(jsonFile):
 
     (values, frontFeatures, backFeatures) = siftFlann.loadValidationSet(masterFolder, DOWNSCALE_RATIO)
 
-    for i in range(len(data)):
-        if '{}'.format(i) in data:
-            image = cv.imread(data['{}'.format(i)]['front'],cv.IMREAD_GRAYSCALE)
-            image = siftFlann.downScale(image, DOWNSCALE_RATIO)
 
-            (keypoints, descriptors) = siftFlann.getSIFTFeatures(image)
+    image = cv.imread(data['{}'.format(i)]['front'],cv.IMREAD_GRAYSCALE)
+    image = siftFlann.downScale(image, DOWNSCALE_RATIO)
 
-            value = siftFlann.getBillValue(descriptors, values, frontFeatures, backFeatures)
+    (keypoints, descriptors) = siftFlann.getSIFTFeatures(image)
 
-            if(data['{}'.format(i)]["value"] == value):
-                properMatches = properMatches + 1
-            else:
-                improperMatches = improperMatches + 1
-                cv.imshow(data['{}'.format(i)]['front'], image)
+    value = siftFlann.getBillValue(descriptors, values, frontFeatures, backFeatures)
 
-    for i in range(len(data)):
-        if '{}'.format(i) in data:
-            image = cv.imread(data['{}'.format(i)]['back'],cv.IMREAD_GRAYSCALE)
-            image = siftFlann.downScale(image, DOWNSCALE_RATIO)
+    if(data['{}'.format(i)]["value"] == value):
+        properMatches = properMatches + 1
+    else:
+        improperMatches = improperMatches + 1
+        cv.imshow(data['{}'.format(i)]['front'], image)
 
-            (keypoints, descriptors) = siftFlann.getSIFTFeatures(image)
-
-            value = siftFlann.getBillValue(descriptors, values, frontFeatures, backFeatures)
-
-            if(data['{}'.format(i)]["value"] == value):
-                properMatches = properMatches + 1
-            else:
-                improperMatches = improperMatches + 1
-                cv.imshow(data['{}'.format(i)]['back'], image)
+  
     
-    print("proper matches: " + str(properMatches))
-    print("improper matches: " + str(improperMatches))
-
+  
 if __name__ == '__main__':
-    validateSift('_BACKUP_data.json')
+
 
     if cv.waitKey(0) & 0xff == 27:
         cv.destroyAllWindows()
