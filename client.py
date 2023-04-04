a3cc915f7e459b51923f28bd0250509750be8b2f
Dakota C <45215796+2018dakotac@users.noreply.github.com>
2023-04-03 21:55:09 -0700

    socketio for webserver

---
+++ client.py
@@ -0,0 +1,25 @@
+import json
+import socketio
+import subprocess
+
+# Initialize the Socket.io client
+sio = socketio.Client()
+
+# Define a callback function to handle the 'json' event
+@sio.on('json')
+def handle_json(json_data):
+    data = json.loads(json_data)
+    print(f'Received JSON data: {data}')
+
+    # Call the control.py script with the JSON data as an argument
+    result = subprocess.run(['python', 'control.py', json_data], capture_output=True, text=True)
+
+    # Send the result back to the server
+    sio.emit('result', result.stdout.strip())
+
+# Connect to the server
+sio.connect('http://localhost:3001')
+
+# Wait for responses from the server
+print("waiting on port 3001")
+sio.wait()
