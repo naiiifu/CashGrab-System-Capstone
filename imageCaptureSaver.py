804b188ca28ba0a8c44f43223e62f3b3f3c29f97
DC <2018dakotac>
2023-06-08 22:20:50 -0700

    newcam

--- imageCaptureSaver.py
+++ imageCaptureSaver.py
@@ -2,22 +2,35 @@ import cv2 as cv
 import os
 import numpy as np
 
-from picamera import PiCamera
-from picamera.array import PiRGBArray
+#from picamera import PiCamera
+#from picamera.array import PiRGBArray
+from picamera2 import Picamera2  
+import time
 
-imageCount = 10
+imageCount = 69
+WARM_UP_TIME = 0.25
 
-camera = PiCamera()
-camera.resolution = (1920,1080)
+camera = Picamera2()
+#camera.resolution = (1920,1080)
+#full_res=camera.sensor_resolution
+#half_res=tuple([dim // 2 for dim in camera.sensor_resolution])
+#still_config=camera.create_still_configuration(main={"size":(720,720),"format":"RGB888"}, raw={"size":full_res})
+#camera.start_preview(Preview.QTGL, x=100, y=200, width=800, height=600,transform=Transform(hflip=1))
+#camera.resolution =(1296,972)
+#camera.resolution = (1296,972)
+#camera.configure(still_config)
+camera.start()
 
 def CaptureImage():
-	rawCapture = PiRGBArray(camera)
-	camera.capture(rawCapture, format="rgb")
-	frame = rawCapture.array
 	
-	frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
 	
-	return frame
+	time.sleep(WARM_UP_TIME)
+	array = camera.capture_array("main")
+	array = np.array(array, dtype=np.uint8)
+	array = cv.cvtColor(array, cv.COLOR_BGR2RGB)
+	
+	
+	return array
 	
 	
 	# output = np.empty(camera.resolution, dtype=np.uint8)
@@ -48,18 +61,18 @@ if __name__ == '__main__':
 	endLoop = False
 	while not endLoop:
 		frame = CaptureImage()
-		print("1")
+		#print("1")
 		# deglared = DeglareImage(frame)
-		print("5")
+		#print("5")
 		cv.imshow("Frame", frame)
 		
 		key = cv.waitKey(0)
 		
 		print(key)
 		
-		if key == 99:
-			cv.imwrite('{}.png'.format(imageCount), frame)
+		if key == 99: #lowecase c
+			cv.imwrite('dfake{}.png'.format(imageCount), frame)
 			imageCount = imageCount + 1
-		elif key == 27:
+		elif key == 27: #esc
 			endLoop = True
 		cv.destroyAllWindows()
