7ffb60b49f0737993dc5c86eeeb85101debd0b76
Dakota C <45215796+2018dakotac@users.noreply.github.com>
2023-06-28 00:38:00 -0700

    shows img path

--- imageChecker.py
+++ imageChecker.py
@@ -3,20 +3,22 @@ import os, os.path
 from PIL import Image
 import numpy 
 
-
+#code to iterate through images. window pixel points are top left CCW currently
 def showAllImg(path):
     imgs = []
+    paths = []
     valid_images = [".jpg",".gif",".png",".tga"]
     for f in os.listdir(path):
         ext = os.path.splitext(f)[1]
         if ext.lower() not in valid_images:
             continue
+        paths.append(os.path.join(path,f))
         imgs.append(Image.open(os.path.join(path,f)))
-    for img in imgs:
-        cv2.namedWindow(img, cv2.WINDOW_NORMAL)        # Create window with freedom of dimensions
-        cv2.resizeWindow(img, 800, 600) 
-        opencvImage = cv2.cvtColor(numpy.array(img), cv2.COLOR_RGB2BGR)
-        cv2.imshow(img,opencvImage)
+    for i in range(len(imgs)):
+        cv2.namedWindow(paths[i], cv2.WINDOW_NORMAL)        # Create window with freedom of dimensions
+        cv2.resizeWindow(paths[i], 800, 600) 
+        opencvImage = cv2.cvtColor(numpy.array(imgs[i]), cv2.COLOR_RGB2BGR)
+        cv2.imshow(paths[i],opencvImage)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     
